091d3afdae4d285ac9efb57e6fa480a38c3b3581
jularis <juliuskouame@gmail.com>
2024-02-18 21:59:15 +0000

    updaet

--- resources/views/dons.blade.php
+++ resources/views/dons.blade.php
@@ -28,9 +28,44 @@
             <!-- page-header -->
             <div class="content-wrapper pad-none">
                 <div class="content-inner">
-					<!-- Contact Section -->
-                 
-                    <!-- Contact Section End -->
+						<!-- Sermon Section -->
+                        <section id="section-sermon" class="sermon-section pad-bottom-45">
+                        <div class="container">
+							 <!-- Sermon Main Wrap -->
+							<div class="sermon-main-wrap sermon-list sermon-grid-2 ">
+								<!-- Row -->
+								<div class="row">
+									<!-- Col -->
+                     
+									<!-- Col -->
+									<div class="col-lg-12">
+										<div class="title-wrap margin-bottom-25">
+											<div class="section-title">
+												<span class="sub-title theme-color text-uppercase">{{ $pageTitle }}</span>
+												<h2 class="section-title margin-top-5">CHAQUE DON COMPTE</h2>
+												<span class="border-bottom"></span>
+											</div>
+											<div class="pad-top-15">
+												<p class="margin-bottom-20">Vous voulez soutenir les activités de Semeur d’Avenir, veuillez nous écrire à l'adresse suivante : {{setting('site.ContactEmail4')}}</p>
+												<p class="margin-bottom-20">
+                                                Chaque don que vous faites à Semeur d’Avenir a un impact significatif. C’est bien plus qu’un simple don. C’est semer dans la destinée d’une multitude, participer à la transformation de vies et contribuer à la propagation de l’Évangile.
+                                                </p>	
+                                                <p class="margin-bottom-20">
+                                                N’hésitez pas à communiquer avec nous pour toute autre information ou pour obtenir de l’aide au  +225 XX XX XX XX XX ou à {{setting('site.ContactEmail4')}}
+                                                </p>								
+											</div>
+										</div> 
+                                        									
+									</div>
+									<!-- Col -->
+								</div>
+								<!-- Row -->
+							</div>
+							<!-- Sermon Main Wrap -->
+						</div>
+						<!-- Container -->
+                    </section>
+                    <!-- Sermon Section End -->
  			 									
                 </div>
             </div>
