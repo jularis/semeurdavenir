e69b321063fd400f94014470d9768eea99a4f6e7
jularis <juliuskouame@gmail.com>
2024-02-19 14:43:04 +0000

    update

--- resources/views/layouts/404.blade.php
+++ resources/views/layouts/404.blade.php
@@ -1,10 +1,60 @@
-<header class="page-header" data-background="{{ asset('public/img/page-header.jpg') }}">
-        <div class="container">
-          <h2>Ooops! Page introuvable</h2>
-          <div class="bosluk333"></div>
-          <p>Il semble que rien n'ait été trouvé à cet endroit. Essayez peut-être l'un des liens ci-dessous ou une recherche ?</p><br>
-          <p><a href="{{url('') }}" class="custom-button"><strong>RETOUR À LA PAGE D'ACCUEIL</strong></a> </p>
-        </div>
-        <!-- end container --> 
-      </header>
+   <!-- page-header -->
+   <div class="page-title-wrap typo-white">
+                <div class="page-title-wrap-inner section-bg-img" data-bg="{{ asset('public/assets/images/page-title.jpg') }}">
+					<span class="theme-overlay"></span>
+                    <div class="container">
+                        <div class="row text-center">
+                            <div class="col-md-12">
+                                <div class="page-title-inner">
+									<div id="breadcrumb" class="breadcrumb margin-bottom-10">
+                                        <a href="{{ url('') }}" class="theme-color">Accueil</a> 
+                                    </div>
+                                    <h1 class="page-title mb-0">{{$pageTitle}}</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- page-header -->
+            <div class="content-wrapper pad-none">
+                <div class="content-inner">
+						<!-- Sermon Section -->
+                        <section id="section-sermon" class="sermon-section pad-bottom-45">
+                        <div class="container">
+							 <!-- Sermon Main Wrap -->
+							<div class="sermon-main-wrap sermon-list sermon-grid-2 ">
+								<!-- Row -->
+								<div class="row">
+									<!-- Col -->
+                     
+									<!-- Col -->
+									<div class="col-lg-12">
+                  <div class="title-wrap margin-bottom-25">
+											<div class="section-title">
+												<span class="sub-title theme-color text-uppercase">{{ $pageTitle }}</span>
+												<h2 class="section-title margin-top-5">Ooops! Page introuvable</h2>
+												<span class="border-bottom"></span>
+											</div>
+											<div class="pad-top-15">
+												<p class="margin-bottom-20">Il semble que rien n'ait été trouvé à cet endroit. Essayez peut-être l'un des liens ci-dessous ou une recherche ?</p>
+												<p class="margin-bottom-20"><a href="{{url('') }}" class="custom-button"><strong>RETOUR À LA PAGE D'ACCUEIL</strong></a>
+                                                </p>	
+                                                 							
+											</div>
+										</div> 
+
+            </div>
+									<!-- Col -->
+								</div>
+								<!-- Row -->
+							</div>
+							<!-- Sermon Main Wrap -->
+						</div>
+						<!-- Container -->
+                    </section>
+                    <!-- Sermon Section End -->
+ 			 									
+                </div>
+            </div>           
  
